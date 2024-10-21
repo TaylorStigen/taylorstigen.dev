ad7c25517ec861dfa6c283503e1ac30ff8005aff
Taylor Stigen <106631704+TaylorStigen@users.noreply.github.com>
2024-10-21 17:25:48 -0600

    Adjust layout
    Add simpleicons support
    Adjust theme colours

---
+++ catalogs/catalog.ts
@@ -0,0 +1,11 @@
+// deno-lint-ignore-file no-explicit-any
+export interface Catalog<T = any> {
+    /** The name of the catalog */
+    name: string;
+  
+    /** Get the SVG source of an icon by name */
+    get(name: string, options?: any): string | Promise<string>;
+  
+    /** Get info from a icon */
+    info(name: string): T | Promise<T>;
+  }
---
+++ lume_icon_plugins/simpleicons.ts
@@ -0,0 +1,29 @@
+import simpleicons, { SimpleIcon } from "../catalogs/simpleicons.ts";
+
+export interface Options {
+  /** Filter name */
+  name: string;
+}
+
+export const defaults: Options = {
+  name: "simpleicons",
+};
+
+export default function plugin(userOptions?: Partial<Options>) {
+  const options = { ...defaults, ...userOptions };
+  const catalog = simpleicons();
+
+  return (site: Lume.Site) => {
+    site.filter(options.name, (name: string, info?: keyof SimpleIcon) => {
+      if (!name) {
+        return "";
+      }
+
+      if (info) {
+        return catalog.info(name)[info];
+      }
+
+      return catalog.get(name);
+    }, true);
+  };
+}
