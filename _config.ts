7667101fd881c492a2ea1bbd9b3f2294f63e5490
Taylor Stigen <106631704+TaylorStigen@users.noreply.github.com>
2025-02-24 14:12:25 -0600

    Fully overhaul site with new template

--- _config.ts
+++ _config.ts
@@ -1,17 +1,13 @@
 import lume from "lume/mod.ts";
-import sass from "lume/plugins/sass.ts";
 import terser from "lume/plugins/terser.ts";
 import svgo from "lume/plugins/svgo.ts";
-import simpleIcons from "./lume_icon_plugins/simpleicons.ts";
 
 const site = lume();
 
 site.ignore("README.md");
 site.data("currentDate", new Date().getFullYear());
-site.use(sass());
 site.use(terser());
 site.use(svgo());
-site.copy([".webp", ".png", ".ico", ".webmanifest"]);
-site.use(simpleIcons());
+site.copy([".webp", ".png", ".ico", ".webmanifest", ".css", ".jpg", ".eot", ".ttf", ".woff", ".woff2"]);
 
 export default site;
