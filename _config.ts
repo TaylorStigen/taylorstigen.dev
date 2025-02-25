af14d45961874149ae4d66e26fe13192618bb255
Taylor Stigen <106631704+TaylorStigen@users.noreply.github.com>
2025-02-24 18:26:19 -0600

    Add LightningCSS and PurgeCSS plugins

--- _config.ts
+++ _config.ts
@@ -1,13 +1,31 @@
 import lume from "lume/mod.ts";
 import terser from "lume/plugins/terser.ts";
 import svgo from "lume/plugins/svgo.ts";
+import lightningCss from "lume/plugins/lightningcss.ts";
+import purgecss from "lume/plugins/purgecss.ts";
 
 const site = lume();
 
 site.ignore("README.md");
 site.data("currentDate", new Date().getFullYear());
-site.use(terser());
+site.use(terser({
+  options: {
+    module: false,
+  },
+}));
 site.use(svgo());
-site.copy([".webp", ".png", ".ico", ".webmanifest", ".css", ".jpg", ".eot", ".ttf", ".woff", ".woff2"]);
+site.copy([
+  ".webp",
+  ".png",
+  ".ico",
+  ".webmanifest",
+  ".jpg",
+  ".eot",
+  ".ttf",
+  ".woff",
+  ".woff2",
+]);
+site.use(purgecss());
+site.use(lightningCss({includes: false}));
 
 export default site;
